40cc07b6ca9e047ea2261285dc023a0101790845
Olga <olgaingvar@gmail.com>
2023-02-10 13:44:58 +0200

    ...

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,10 @@
-﻿double numberA = 12;
+﻿/*double numberA = 12;
 double numberB = 5;
-System.Console.WriteLine(numberA / numberB);
+System.Console.WriteLine(numberA / numberB);*/
 
+int numberA = new Random().Next(1, 10);
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
+int result = numberA + numberB;
+System.Console.WriteLine(result);
