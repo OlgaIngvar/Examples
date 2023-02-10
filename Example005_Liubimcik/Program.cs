95bcfd24eb31027d50d017205ebadc3ab551dd83
Olga <olgaingvar@gmail.com>
2023-02-10 14:37:52 +0200

    Любимчик

---
+++ Example005_Liubimcik/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это снова Маша");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
